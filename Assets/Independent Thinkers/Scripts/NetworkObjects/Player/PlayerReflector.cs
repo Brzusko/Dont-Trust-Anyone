3555bb53401619ac0a881e3ca93ad882dca63f6c
w.brzuszkiewicz <wojciech.brzuszkiewicz@hotmail.com>
2021-04-22 23:30:44 +0200

    Reflection optimize tweak

--- Assets/Independent Thinkers/Scripts/NetworkObjects/Player/PlayerReflector.cs
+++ Assets/Independent Thinkers/Scripts/NetworkObjects/Player/PlayerReflector.cs
@@ -7,6 +7,7 @@ public class PlayerReflector : NetworkBehaviour
 {
     private PlayerInputSystem _pis;
     private SpriteRenderer _spriteRenderer;
+    private LookDirection _lastDirection;
 
     private void Start() {
         _pis = GetComponent<PlayerInputSystem>();
@@ -15,14 +16,20 @@ public class PlayerReflector : NetworkBehaviour
 
     [ServerCallback]
     private void Update() {
+        if(_pis.pointerDir == _lastDirection)
+            return;
+
         if(_pis.pointerDir == LookDirection.LEFT)
             Reflect(true);
         else
             Reflect(false);
+
+        _lastDirection = _pis.pointerDir;
     }
 
     [ClientRpc]
     private void Reflect(bool reflection) {
-        if(_spriteRenderer != null) _spriteRenderer.flipX = reflection;
+        if(_spriteRenderer == null) return;
+        _spriteRenderer.flipX = reflection;
     }
 }
