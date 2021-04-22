7ba36b69da2ae6c321aa66d5f0299d5793c3d70a
w.brzuszkiewicz <wojciech.brzuszkiewicz@hotmail.com>
2021-04-22 17:00:10 +0200

    Server side basic sync

--- Assets/Independent Thinkers/Scripts/NetworkObjects/Player/PlayerBasicMovement.cs
+++ Assets/Independent Thinkers/Scripts/NetworkObjects/Player/PlayerBasicMovement.cs
@@ -6,13 +6,14 @@ public class PlayerBasicMovement : NetworkBehaviour
 {
     private PlayerInputSystem _pis;
 
-    public override void OnStartLocalPlayer()
+    public override void OnStartServer()
     {
         _pis = GetComponent<PlayerInputSystem>();
     }
 
+    [ServerCallback]
     private void Update() {
         if(_pis == null) return;
-        transform.position = transform.position + ((Vector3)_pis.Velocity * Time.deltaTime * 1.5f);
+        transform.position = transform.position + ((Vector3)_pis.s_movementInput * Time.deltaTime * 1.5f);
     }
 }
--- Assets/Independent Thinkers/Scripts/NetworkObjects/Player/PlayerInputSystem.cs
+++ Assets/Independent Thinkers/Scripts/NetworkObjects/Player/PlayerInputSystem.cs
@@ -4,18 +4,40 @@ using Mirror;
 
 public class PlayerInputSystem : NetworkBehaviour
 {
-    public Vector2 Velocity {get; private set;}
+    #region ServerVars // s_ stands for server
+    public Vector2 s_pointerPos {get; private set;}
+    public Vector2 s_movementInput {get; private set;}
+    #endregion
+    public Vector2 LocalVelocity {get; private set;}
     public Vector2 PointerPos {get; private set;}
     private PlayerInput _playerInput;
     private InputAction _movementAction;
     private InputAction _pointerAction;
-
     private Camera _mainCamera;
 
+    #region Server Methods
+    #endregion
+
+    #region Client Methods
+    [Command]
+    private void ReciveMovementInput(Vector2 unitVector) {
+        Debug.Log("Reciving movement input from client");
+        if (unitVector == s_movementInput) return;
+        s_movementInput = unitVector;
+    }
+
+    [Command]
+    private void ReciveMouseInput(Vector2 mousePos) {
+        Debug.Log("Reciving mouse input from client");
+        if (mousePos == s_pointerPos) return;
+        s_pointerPos = mousePos;
+    }
+    #endregion
     public override void OnStartLocalPlayer() {
         CSetup();
     }
 
+    [Client]
     private void CSetup() {
         _playerInput = GetComponent<PlayerInput>();
         var playerActions = _playerInput.actions;
@@ -23,15 +45,22 @@ public class PlayerInputSystem : NetworkBehaviour
         _movementAction = playerActions.FindAction("Movement");
         _mainCamera = Camera.main;
 
-        if(_pointerAction != null) {
+        if(_pointerAction != null && _movementAction != null) {
             _pointerAction.performed += ctx => {
                 PointerPos = _mainCamera.ScreenToWorldPoint(ctx.ReadValue<Vector2>());
+                ReciveMouseInput(PointerPos);
             };
-        }
-    }
-    private void Update() {
-        if(isLocalPlayer) {
-            Velocity = _movementAction.ReadValue<Vector2>();
+            _movementAction.performed += ctx => {
+                var unitVector = _movementAction.ReadValue<Vector2>();
+                if (unitVector == LocalVelocity)
+                    return;
+                LocalVelocity = unitVector;
+                ReciveMovementInput(LocalVelocity);
+            };
+            _movementAction.canceled += ctx => {
+                LocalVelocity = Vector2.zero;
+                ReciveMovementInput(LocalVelocity);
+            };
         }
     }
 }
